845bbe5dbff8e749086ae6bcbea4b55317f6daba
pingheng <pingheng@corp.netease.com>
2020-01-14 15:05:24 +0800

    detect

--- detect.sh
+++ detect.sh
@@ -2,5 +2,6 @@ python detect.py \
     --classes ./wider_face/wider_face.names \
     --num_classes 2 \
     --yolo_max_boxes 10 \
-    --weights ./checkpoints/yolov3_train_3.tf \
+    --yolo_score_threshold 0.1 \
+    --weights ./checkpoints/yolov3_train_5.tf \
     --image ./data/girl.png
